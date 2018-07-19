f712dce0e795a801913ab5857c2bfac2f5ea4a9e
lamloei <lamloei.com@gmail.com>
2018-07-20 00:00:49 +0700

    nbiot true

---
+++ lamloei_node32s_bc95_echo_true/lamloei_node32s_bc95_echo_true.ino
@@ -0,0 +1,27 @@
+#include <HardwareSerial.h>
+#define RESET_PIN 33
+
+HardwareSerial Serial2(2);
+
+void setup() {
+  pinMode(RESET_PIN, OUTPUT);
+  digitalWrite(RESET_PIN, LOW);
+  delay(1);
+  digitalWrite(RESET_PIN, HIGH);  
+  Serial.begin(115200);
+  Serial2.begin(9600, SERIAL_8N1, 14, 27);
+}
+
+void loop() {
+  char c;
+
+  if ( Serial.available() ) {
+    c = Serial.read();
+    Serial.print(c);
+    Serial2.print(c);
+  }
+  if ( Serial2.available() ) {
+    c = Serial2.read();
+    Serial.print(c);
+  }
+}
