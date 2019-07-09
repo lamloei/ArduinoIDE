88f545d87ec7956893e8d52e7450a5663738838d
lamloei <lamloei@users.noreply.github.com>
2019-07-09 15:18:50 +0700

    Update lamloei_node32pico_bc95_echo_ais.ino

--- lamloei_node32pico_bc95_echo_ais/lamloei_node32pico_bc95_echo_ais.ino
+++ lamloei_node32pico_bc95_echo_ais/lamloei_node32pico_bc95_echo_ais.ino
@@ -9,7 +9,7 @@ void setup() {
   delay(1);
   digitalWrite(RESET_PIN, LOW);  
   Serial.begin(115200);
-  Serial1.begin(9600, SERIAL_8N1, 14, 27);
+  Serial1.begin(9600, SERIAL_8N1 /*0x06*/, 14, 27);
 }
 
 void loop() {
